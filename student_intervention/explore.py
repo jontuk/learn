7d9e5f4002c4987898d6c4474217b8743c2b0196
jon <jon@jpt.me.uk>
2017-01-17 06:46:46 +0000

    .

--- student_intervention/explore.py
+++ student_intervention/explore.py
@@ -1,5 +1,9 @@
 
+# Import libraries
+import numpy as np
 import pandas as pd
+from time import time
+from sklearn.metrics import f1_score
 
 student_data = pd.read_csv("student-data.csv")
 
@@ -44,7 +48,7 @@ X_all = preprocess_features(X_all)
 #------------------------------------------
 
 # TODO: Import any additional functionality you may need here
-from sklearn.cross_validation import ShuffleSplit
+import sklearn.cross_validation
 
 # TODO: Set the number of training points
 num_train = 300
@@ -53,12 +57,57 @@ num_train = 300
 num_test = X_all.shape[0] - num_train
 
 # TODO: Shuffle and split the dataset into the number of training and testing points above
-X_train = None
-X_test = None
-y_train = None
-y_test = None
+
+train_idxs, test_idxs \
+    = sklearn.cross_validation.ShuffleSplit(n=X_all.shape[0], test_size=num_test, n_iter=1, random_state=1)\
+    .__iter__().next()
+
+X_train = X_all.take(train_idxs)
+X_test = X_all.take(test_idxs)
+y_train = y_all.take(train_idxs)
+y_test = y_all.take(test_idxs)
 
 # Show the results of the split
-#print "Training set has {} samples.".format(X_train.shape[0])
-#print "Testing set has {} samples.".format(X_test.shape[0])
+print("Training set has {} samples.".format(X_train.shape[0]))
+print("Testing set has {} samples.".format(X_test.shape[0]))
 
+#----------------------------------------------------
+
+
+def train_classifier(clf, X_train, y_train):
+    ''' Fits a classifier to the training data. '''
+
+    # Start the clock, train the classifier, then stop the clock
+    start = time()
+    clf.fit(X_train, y_train)
+    end = time()
+
+    # Print the results
+    print("Trained model in {:.4f} seconds".format(end - start))
+
+
+def predict_labels(clf, features, target):
+    ''' Makes predictions using a fit classifier based on F1 score. '''
+
+    # Start the clock, make predictions, then stop the clock
+    start = time()
+    y_pred = clf.predict(features)
+    end = time()
+
+    # Print and return results
+    print("Made predictions in {:.4f} seconds.".format(end - start))
+    return f1_score(target.values, y_pred, pos_label='yes')
+
+
+def train_predict(clf, X_train, y_train, X_test, y_test):
+    ''' Train and predict using a classifer based on F1 score. '''
+
+    # Indicate the classifier and the training set size
+    print("Training a {} using a training set size of {}. . .".format(clf.__class__.__name__, len(X_train)))
+
+    # Train the classifier
+    train_classifier(clf, X_train, y_train)
+
+    # Print the results of prediction for both training and testing
+    print("F1 score for training set: {:.4f}.".format(predict_labels(clf, X_train, y_train)))
+    print("F1 score for test set: {:.4f}.".format(predict_labels(clf, X_test, y_test)))
