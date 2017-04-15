cd511694dd2fce35cf26fc4ed3bcc132a3367a06
jon <jon@jpt.me.uk>
2017-04-15 08:50:46 +0100

    .

--- customer_segments/explore.py
+++ customer_segments/explore.py
@@ -7,7 +7,8 @@ df = pd.DataFrame.from_dict(d)
 
 df = pd.DataFrame(index=[0, 1, 2], columns=['a', 'b', 'c'], data=[[10, 20, 30], [100, 200, 300], [1, 2, 3]])
 
+print(df.quantile(0.25))
 
-print(df)
-print(np.log(df))
-print(np.power(np.e, np.log(df)))
+print(df.quantile(0.75))
+
+print(np.percentile(df['a'], 25))
