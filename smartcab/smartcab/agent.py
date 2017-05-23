b626d2e2990f84c56680ef6c57b62543648ab12c
Jon Turner <jon@jpt.me.uk>
2017-05-23 20:41:13 +0100

    .

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -45,7 +45,7 @@ class LearningAgent(Agent):
         # If 'testing' is True, set epsilon and alpha to 0
         if self.learning:
             self.trial_num += 1
-            self.epsilon = math.pow(0.9, self.trial_num)
+            self.epsilon = math.pow(math.e, -0.05 * self.trial_num)
         else:
             self.epsilon = 0
 
@@ -150,7 +150,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, learning=True, epsilon=1, alpha=0.75)
+    agent = env.create_agent(LearningAgent, learning=True, epsilon=1, alpha=.33)
     
     ##############
     # Follow the driving agent
@@ -172,7 +172,7 @@ def run():
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=5, tolerance=0.05)
+    sim.run(n_test=10, tolerance=0.01)
 
 
 if __name__ == '__main__':
